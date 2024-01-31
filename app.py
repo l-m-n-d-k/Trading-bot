221d730020585f96a3da2d3223da32c9b06d3fbb
l-m-n-d-k <dima2642007@gmail.com>
2024-01-31 10:34:12 +0300

    finish line - add graph

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 from openai import OpenAI
+import requests
 
 app = Flask(__name__)
 
 
 def generate_text(prompt):
     try:
-        client = OpenAI(api_key='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')
+        client = OpenAI(api_key='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')
         response = client.chat.completions.create(
 timeout=60,
 model="ft:gpt-3.5-turbo-1106:t1::8mJFSyqI",
@@ -15,7 +16,20 @@ messages=[
                 {"role": "user", "content": prompt}
             ],
 )
-        return response.choices[0].message.content
+        return float(response.choices[0].message.content.split()[-1]) + 0.21 * get_btc_price()
+    except Exception as e:
+        return str(e)
+    
+binance_api_url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
+
+def get_btc_price():
+    try:
+        response = requests.get(binance_api_url)
+        if response.status_code == 200:
+            data = response.json()
+            return float(data['price'])
+        else:
+            return None
     except Exception as e:
         return str(e)
 
